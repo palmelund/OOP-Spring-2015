06a3d48ab65f1e060362b42079f8b63e15d1a46b
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-05 19:07:15 +0200

    Created Product

--- Product.cs
+++ Product.cs
@@ -8,5 +8,49 @@ namespace OOP_Spring_2015
 {
     class Product
     {
+
+        public uint ProductID
+        {
+            get;
+            protected set;
+        }
+
+        public string Name
+        {
+            get;
+            protected set;
+        }
+
+        public uint Price
+        {
+            get;
+            protected set;
+        }
+
+        public bool Active
+        {
+            get;
+            protected set;
+        }
+
+        public bool CanBeBoughtOnCredit
+        {
+            get;
+            protected set;
+        }
+
+        public Product(uint productID, string name, uint price, bool active, bool credit)
+        {
+            ProductID = productID;
+            Name = name;
+            Price = price;
+            Active = active;
+            CanBeBoughtOnCredit = credit;
+        }
+
+        protected Product()
+        {
+
+        }
     }
 }
