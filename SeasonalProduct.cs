cd021467659f004e6c9107986e596565b4426cd0
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-05 19:13:29 +0200

    Added missing file

---
+++ SeasonalProduct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Spring_2015
+{
+    class SeasonalProduct : Product
+    {
+        public DateTime SeasonStartDate
+        {
+            get;
+            protected set;
+        }
+
+        public DateTime SeasonEndDate
+        {
+            get;
+            protected set;
+        }
+
+        public SeasonalProduct(uint productID, string name, uint price, bool active, bool credit, DateTime seasonStartDate, DateTime seasonEndDate)
+        {
+            ProductID = productID;
+            Name = name;
+            Price = price;
+            Active = active;
+            CanBeBoughtOnCredit = credit;
+            SeasonStartDate = seasonStartDate;
+            SeasonEndDate = seasonEndDate;
+        }
+    }
+}
