49d20dd28dc4cb3ecec5974c5793c8bb0dd2a327
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-06 10:57:30 +0200

    Added test project

--- OOP-Spring-2015-Test/UserTest.cs
+++ OOP-Spring-2015-Test/UserTest.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using OOP_Spring_2015;
 
 namespace OOP_Spring_2015.Test
 {
-    
+
     [TestFixture]
     class TestUser
     {
@@ -16,12 +17,13 @@ namespace OOP_Spring_2015.Test
         {
             bool success1 = true;
             bool success2 = true;
+            bool success3 = true;
 
             try
             {
-                User user1 = new User(1, "", "palmelund", "thepalmelund", "frederik.palmelund@gmail.com");
+                User user1 = new User(1, "frederik", "palmelund", "thepalmelund", "frederik.palmelund@gmail.com");
             }
-            catch(Exception)
+            catch (Exception)
             {
                 success1 = false;
             }
@@ -35,8 +37,18 @@ namespace OOP_Spring_2015.Test
                 success2 = false;
             }
 
+            try
+            {
+                User user3 = new User(1, "", "palmelund", "thepalmelund", "frederik.palmelund@gmail.com");
+            }
+            catch (Exception)
+            {
+                success3 = false;
+            }
+
             Assert.AreEqual(true, success1);
             Assert.AreEqual(false, success2);
+            Assert.AreEqual(false, success3);
         }
     }
 }
